250a0903a2cc289991e2e315da16e09b6f3c2023
Drake B <drakeboard719@riversideunified.org>
2024-03-20 13:30:18 -0700

    Added timer and scene fader

--- 3D FPS Project/Assets/Models/Scripts/Timer.cs
+++ 3D FPS Project/Assets/Models/Scripts/Timer.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using System;
 
 public class Timer : MonoBehaviour
 {
+    public float timeRemaining = 180f;
+    public bool timerIsRunning = false;
+    public TextMeshProUGUI timerText;
+    
+    private Vector3 _startingPosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //timerIsRunning = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(timerIsRunning)
+        {
+        if(timeRemaining > 0)
+        {
+            timeRemaining -= Time.deltaTime;
+            DisplayTime(timeRemaining);
+        }
+        else
+        {
+            timeRemaining = 0;
+            timerIsRunning = false;
+        }
+    }
+
+    void DisplayTime(float timeToDisplay)
+    {
+        timeToDisplay += 1;
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);    
+    }
+
+    void StartGameTimer()
+    {
+        timerIsRunning = true;
     }
 }
+}
+

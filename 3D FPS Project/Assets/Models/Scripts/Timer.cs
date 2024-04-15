ca25ce49c7efc06233d05eca5189394332109815
Drake B <drakeboard719@riversideunified.org>
2024-04-15 15:16:57 -0700

    Worked on scenes

--- 3D FPS Project/Assets/Models/Scripts/Timer.cs
+++ 3D FPS Project/Assets/Models/Scripts/Timer.cs
@@ -45,6 +45,11 @@ public class Timer : MonoBehaviour
         _isTimerRunning = false;
     }
 
+    public float GetTimeRemaining()
+    {
+        return timeRemaining;
+    }
+
     private void DisplayTime(float timetoDisplay)
     {
         timetoDisplay += 1;
